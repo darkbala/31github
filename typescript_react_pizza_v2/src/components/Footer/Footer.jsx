532fa95ada34c548b9a8eeb0b5d459416a43893e
darkbala <rustam.samsaev@gmail.com>
2023-12-05 23:45:22 +0600

    5/31 process

--- typescript_react_pizza_v2/src/components/Footer/Footer.jsx
+++ typescript_react_pizza_v2/src/components/Footer/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import cla from './Footer.module.scss';
 
 function Footer() {
-  return <div className={cla.wrap}>Footer</div>;
+  return (
+    <div className={cla.wrap}>
+      <h1>Footer</h1>
+    </div>
+  );
 }
 
 export default Footer;
