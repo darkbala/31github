67d3fae25bc106b943de9aa4be4b13a9a950d375
darkbala <rustam.samsaev@gmail.com>
2023-12-05 23:43:36 +0600

    5/31 process

--- typescript_react_pizza_v2/src/components/Contact/Contact.jsx
+++ typescript_react_pizza_v2/src/components/Contact/Contact.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import cla from './Contact.module.scss';
 
 function Contact() {
-  return <div className={cla.wrap}>Contact</div>;
+  return (
+    <div className={cla.wrap}>
+      <h1>Contacts</h1>
+    </div>
+  );
 }
 
 export default Contact;
