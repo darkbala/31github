2e3180950b28fdb54fc9195ccd17b708e2b5de92
darkbala <rustam.samsaev@gmail.com>
2023-12-05 23:44:11 +0600

    5/31 process

--- typescript_react_pizza_v2/src/components/Form/Form.jsx
+++ typescript_react_pizza_v2/src/components/Form/Form.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import cla from './Form.module.scss';
 
 function Form() {
-  return <div className={cla.wrap}>Form</div>;
+  return (
+    <div className={cla.wrap}>
+      <h1>Form</h1>
+    </div>
+  );
 }
 
 export default Form;
