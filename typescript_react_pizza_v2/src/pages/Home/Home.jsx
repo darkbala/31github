57ebd0935635c3f4d64d61ce8857305078b36858
darkbala <rustam.samsaev@gmail.com>
2023-12-05 23:44:33 +0600

    5/31 process

--- typescript_react_pizza_v2/src/pages/Home/Home.jsx
+++ typescript_react_pizza_v2/src/pages/Home/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import cla from './Home.module.scss';
 
 function Home() {
-  return <div className={cla.wrap}>Home</div>;
+  return (
+    <div className={cla.wrap}>
+      <h1>Home</h1>
+    </div>
+  );
 }
 
 export default Home;
