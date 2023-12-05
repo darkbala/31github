86f9e0afd6331cbd030847f4ecdb7507cbf82edd
darkbala <rustam.samsaev@gmail.com>
2023-12-05 23:45:06 +0600

    5/31 process

--- typescript_react_pizza_v2/src/components/Header/Header.jsx
+++ typescript_react_pizza_v2/src/components/Header/Header.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import cla from './Header.module.scss';
 
 function Header() {
-  return <div className={cla.wrap}>Header</div>;
+  return (
+    <div className={cla.wrap}>
+      <h1>Header</h1>
+    </div>
+  );
 }
 
 export default Header;
