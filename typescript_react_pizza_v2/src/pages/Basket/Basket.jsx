ded19fd582a826032e9b0230c418e4ded154bcf5
darkbala <rustam.samsaev@gmail.com>
2023-12-05 23:44:50 +0600

    5/31 process

--- typescript_react_pizza_v2/src/pages/Basket/Basket.jsx
+++ typescript_react_pizza_v2/src/pages/Basket/Basket.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import cla from './Basket.module.scss';
 
 function Basket() {
-  return <div className={cla.wrap}>Basket</div>;
+  return (
+    <div className={cla.wrap}>
+      <h1>Basket</h1>
+    </div>
+  );
 }
 
 export default Basket;
