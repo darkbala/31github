59be8bf571124e8a62820b427c095cf3c16bf73a
darkbala <rustam.samsaev@gmail.com>
2023-12-07 00:23:55 +0600

    6/31 process

--- typescript_react_pizza_v2/src/pages/Home/Home.jsx
+++ typescript_react_pizza_v2/src/pages/Home/Home.jsx
@@ -5,6 +5,11 @@ function Home() {
   return (
     <div className={cla.wrap}>
       <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet temporibus culpa, totam
+        delectus natus possimus ducimus accusamus voluptates tempora doloremque rerum mollitia
+        voluptatum eum atque unde deserunt quidem ipsam ipsum?
+      </p>
     </div>
   );
 }
