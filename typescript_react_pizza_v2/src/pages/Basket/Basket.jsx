4f6d2e8fa3e4e6c14abbd81e26cab9b471130dcc
darkbala <rustam.samsaev@gmail.com>
2023-12-07 00:24:53 +0600

    6/31 process

--- typescript_react_pizza_v2/src/pages/Basket/Basket.jsx
+++ typescript_react_pizza_v2/src/pages/Basket/Basket.jsx
@@ -5,6 +5,11 @@ function Basket() {
   return (
     <div className={cla.wrap}>
       <h1>Basket</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque voluptate minima qui
+        possimus, deleniti expedita numquam error quos quasi? Quod hic aperiam cupiditate velit,
+        mollitia voluptas provident placeat voluptatum atque!
+      </p>
     </div>
   );
 }
