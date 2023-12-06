50d8db1c9856f7f0c2321d616903dec9e2a22500
darkbala <rustam.samsaev@gmail.com>
2023-12-07 00:24:24 +0600

    6/31 process

--- typescript_react_pizza_v2/src/components/Form/Form.jsx
+++ typescript_react_pizza_v2/src/components/Form/Form.jsx
@@ -5,6 +5,11 @@ function Form() {
   return (
     <div className={cla.wrap}>
       <h1>Form</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur, ea eum voluptates
+        dolorum debitis provident earum iure laborum sit molestias id, veritatis dolore
+        voluptatibus. Assumenda libero iusto temporibus voluptatibus inventore!
+      </p>
     </div>
   );
 }
